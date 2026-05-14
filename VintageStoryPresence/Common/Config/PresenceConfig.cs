d0d71b6ba1e57b847416d12221e45be290e93e38
Akross <59298840+akrossu@users.noreply.github.com>
2026-05-14 14:15:15 -0400

    updated: default values

--- VintageStoryPresence/Common/Config/PresenceConfig.cs
+++ VintageStoryPresence/Common/Config/PresenceConfig.cs
@@ -1,10 +1,14 @@
 ﻿namespace VintageStoryPresence.Common.Config;
 
+/// <summary>
+/// Default Config Values
+/// This is set when configlib is not installed
+/// </summary>
 public class PresenceConfig
 {
     public string AppId { get; set; } = "1441987315235946546";
-    public string Details { get; set; } = "PresenceConfig Details";
-    public string State { get; set; } = "PresenceConfig State";
+    public string Details { get; set; } = "In {GameMode} Mode";
+    public string State { get; set; } = "Playing {PlayerModeFormatted}";
     public string LargeImageKey { get; set; } = "game_icon"; 
     public string LargeImageText { get; set; } = "Vintage Story";
     public string SmallImageKey { get; set; } = "";
