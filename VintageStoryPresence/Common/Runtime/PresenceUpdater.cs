845adfdc19fc6a6dc1bc1b8b5cf33b7da236298b
Akross <59298840+akrossu@users.noreply.github.com>
2026-05-14 12:47:29 -0400

    refactor: context building

--- VintageStoryPresence/Common/Runtime/PresenceUpdater.cs
+++ VintageStoryPresence/Common/Runtime/PresenceUpdater.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using DiscordRPC;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
 using VintageStoryPresence.Common.Config;
+using VintageStoryPresence.Common.Presence;
 using VintageStoryPresence.Common.Services;
 
 namespace VintageStoryPresence.Common.Runtime;
@@ -16,23 +19,14 @@ public static class PresenceUpdater
         _listenerId = PresenceCore.Api.Event.RegisterGameTickListener(UpdatePresence, 5000);
     }
 
-    private static void UpdatePresence(float obj)
+    private static void UpdatePresence(float deltaTime)
     {
         try
         {
-            RichPresence presence = new RichPresence
-            {
-                Details = ConfigManager.Config.Details,
-                State = ConfigManager.Config.State,
-                Assets = new Assets
-                {
-                    LargeImageKey = ConfigManager.Config.LargeImageKey,
-                    LargeImageText = ConfigManager.Config.LargeImageText,
-                    SmallImageKey = ConfigManager.Config.SmallImageKey,
-                    SmallImageText = ConfigManager.Config.SmallImageText
-                },
-                Timestamps = new Timestamps(_startTime)
-            };
+            PresenceContext ctx = BuildContext(deltaTime);
+
+            RichPresence presence = PresenceBuilder.Build(ctx);
+            presence.Timestamps = new Timestamps(_startTime);
             
             DiscordRpcService.SetPresence(presence);
         }
@@ -42,6 +36,30 @@ public static class PresenceUpdater
         }
     }
 
+    private static PresenceContext BuildContext(float delta)
+    {
+        ICoreAPI? api = PresenceCore.Api;
+        ICoreClientAPI? capi = PresenceCore.Capi;
+        IWorldAccessor? world = api?.World;
+        
+        int onlinePlayers = world?.AllOnlinePlayers?.Length ?? 0;
+        bool isSinglePlayer = capi?.IsSinglePlayer ?? false;
+        
+        return new PresenceContext
+        {
+            DeltaTime = delta,
+            
+            PlayerName = "Player Name",
+            
+            PlayerCount = (onlinePlayers == 1) ? "1 Player Online" : $"{onlinePlayers} Players Online",
+
+            ServerName = "Server Name",
+            WorldName = "World Name",
+            
+            GameMode = isSinglePlayer ? "Singleplayer" : "Multiplayer"
+        };
+    }
+    
     public static void Dispose()
     {
         PresenceCore.Api.Event.UnregisterGameTickListener(_listenerId);
