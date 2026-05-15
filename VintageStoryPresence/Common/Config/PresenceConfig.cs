8b6bdd016f547f7c5ec1507a97b55805ee79b78a
Akross <59298840+akrossu@users.noreply.github.com>
2026-05-14 21:23:29 -0400

    updated: default config values

--- VintageStoryPresence/Common/Config/PresenceConfig.cs
+++ VintageStoryPresence/Common/Config/PresenceConfig.cs
@@ -7,10 +7,10 @@
 public class PresenceConfig
 {
     public string AppId { get; set; } = "1441987315235946546";
-    public string Details { get; set; } = "In {GameMode} Mode";
-    public string State { get; set; } = "Playing {PlayerModeFormatted}";
+    public string Details { get; set; } = "In {{GameMode}} Mode";
+    public string State { get; set; } = "Playing {{PlayerModeFormatted}}";
     public string LargeImageKey { get; set; } = "game_icon"; 
-    public string LargeImageText { get; set; } = "Vintage Story";
-    public string SmallImageKey { get; set; } = "";
-    public string SmallImageText { get; set; } = "";
+    public string LargeImageText { get; set; } = "Large Image Text Hover";
+    public string SmallImageKey { get; set; } = "gear_icon";
+    public string SmallImageText { get; set; } = "Small Image Text Hover";
 }
